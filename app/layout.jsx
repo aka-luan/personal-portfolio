d6822bce7e5bfe0711926f87048925517c9d8148
Luan Felipe Britto Alves <lfalves.terc@pa.sebrae.com.br>
2023-04-12 17:31:01 -0300

    layout and theme color changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,16 +14,16 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body className={`${nunito.className} bg-white`}>
         <div className='container mx-auto'>
-          <nav className='flex p-6 justify-between items-center text-darkgray font-bold'>
+          <nav className='flex p-6 justify-between items-center text-black font-bold'>
             <div className='flex-1'>
               <ul className='flex'>
-                <li className='mr-6 hover:text-orange transition duration-300 ease-in-out'>
+                <li className='mr-6 hover:text-purple transition duration-300 ease-in-out'>
                   <a href='#'>About me</a>
                 </li>
-                <li className='mr-6 hover:text-orange transition duration-300 ease-in-out'>
+                <li className='mr-6 hover:text-purple transition duration-300 ease-in-out'>
                   <a href='#'>Projects</a>
                 </li>
-                <li className='mr-6 hover:text-orange transition duration-300 ease-in-out'>
+                <li className='mr-6 hover:text-purple transition duration-300 ease-in-out'>
                   <a href='#'>Experience</a>
                 </li>
               </ul>
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
               <Image className='h-40 w-40 absolute' src={logo} alt="white L inside a black circle" />
             </div>
             <div className='flex-1 flex justify-end'>
-              <button className='py-3 px-9 bg-gray rounded-3xl transition duration-300 ease-in-out hover:scale-110 hover:text-white hover:bg-black'>Contact me</button>
+              <button className='py-3 px-9 bg-gray rounded-3xl transition duration-300 ease-in-out hover:scale-110 hover:text-purple hover:bg-black'>Contact me</button>
             </div>
           </nav>
           {children}
