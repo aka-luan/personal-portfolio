f4d78ae120f3ef1599e735c497c5123da7ad5063
Luan Alves <akaluan.la@gmail.com>
2023-04-15 21:14:39 -0300

    responsivity adjustments

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body className={`${nunito.className} bg-white text-xs lg:text-base`}>
         <div className='container mx-auto'>
-          <nav className='flex p-6 justify-between items-center text-black font-bold'>
+          <nav className='flex px-6 pb-6 pt-3 justify-between items-center text-black font-bold'>
             <div className='md:flex-1 hidden md:block'>
               <ul className='flex'>
                 <li className='mr-6 hover:text-purple transition duration-300 ease-in-out'>
@@ -32,10 +32,10 @@ export default function RootLayout({ children }) {
             <div className='md:hidden'>
               <Bars2Icon className='h-8 w-8 text-black cursor-pointer hover:text-purple'></Bars2Icon>
             </div>
-            <div className='md:flex-1 flex justify-center items-center'>
-              <Image className='scale-75 lg:scale-100 h-40 w-40 absolute' src={logo} alt="white L inside a black circle" />
+            <div className='md:flex-1 flex justify-center'>
+              <Image className='h-16 w-16 md:h-20 md:w-20' src={logo} alt="white L inside a black circle" />
             </div>
-            <div className='md:flex-1 flex hidden sm:block justify-end'>
+            <div className='md:flex-1 hidden md:flex justify-end'>
               <button className='py-3 px-9 bg-gray rounded-3xl transition duration-300 ease-in-out hover:scale-110 hover:text-purple hover:bg-black'>Contact me</button>
             </div>
           </nav>
