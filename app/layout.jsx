ecfb12dde4b6d3a0cf777da97258688fa275cfee
Luan Alves <akaluan.la@gmail.com>
2023-04-16 12:22:29 -0300

    added navbar component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,10 @@
 import './globals.css';
 import { Nunito } from 'next/font/google';
-import logo from '/public/assets/logo.svg'
-import Image from 'next/image'
-import { Bars2Icon } from '@heroicons/react/24/outline'
+
+import Navbar from '@/components/Navbar';
 
 const nunito = Nunito({
-  weight: ['400', '700'],
+  weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
   variable: '--font-nunito',
 });
@@ -15,30 +14,7 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body className={`${nunito.className} bg-white text-xs lg:text-base`}>
         <div className='container mx-auto'>
-          <nav className='flex px-6 pb-6 pt-3 justify-between items-center text-black font-bold'>
-            <div className='md:flex-1 hidden md:block'>
-              <ul className='flex'>
-                <li className='mr-6 hover:text-purple transition duration-300 ease-in-out'>
-                  <a href='#'>About me</a>
-                </li>
-                <li className='mr-6 hover:text-purple transition duration-300 ease-in-out'>
-                  <a href='#'>Projects</a>
-                </li>
-                <li className='mr-6 hover:text-purple transition duration-300 ease-in-out'>
-                  <a href='#'>Experience</a>
-                </li>
-              </ul>
-            </div>
-            <div className='md:hidden'>
-              <Bars2Icon className='h-8 w-8 text-black cursor-pointer hover:text-purple'></Bars2Icon>
-            </div>
-            <div className='md:flex-1 flex justify-center'>
-              <Image className='h-16 w-16 md:h-20 md:w-20' src={logo} alt="white L inside a black circle" />
-            </div>
-            <div className='md:flex-1 hidden md:flex justify-end'>
-              <button className='py-3 px-9 bg-gray rounded-3xl transition duration-300 ease-in-out hover:scale-110 hover:text-purple hover:bg-black'>Contact me</button>
-            </div>
-          </nav>
+          <Navbar />
           {children}
         </div>
       </body>
---
+++ components/Navbar.jsx
@@ -0,0 +1,74 @@
+'use client';
+
+import logo from '/public/assets/logo.svg';
+import Image from 'next/image';
+import { Bars2Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Disclosure, Transition } from '@headlessui/react';
+
+const navigation = [
+  { name: 'Home', href: '#', current: true },
+  { name: 'About', href: '#about', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Experience', href: '#', current: false },
+];
+
+export default function Navbar() {
+  return (
+    <Disclosure as='nav'>
+      {({ open }) => (
+        <>
+          <div className='flex px-6 pb-6 pt-3 justify-between items-center text-black font-medium'>
+            <div className='md:flex-1  hidden md:block'>
+              <div className='flex xl:space-x-4'>
+                {navigation.map((item) => (
+                  <a
+                    className='hover:text-purple px-3 py-2 transition duration-300 ease-in-out'
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? 'page' : undefined}>
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div className='md:hidden'>
+              <Disclosure.Button>
+              
+                {open ? (
+                  <XMarkIcon className='h-8 w-8 text-black cursor-pointer hover:text-purple'></XMarkIcon>
+                ) : (
+                  <Bars2Icon className='h-8 w-8 text-black cursor-pointer hover:text-purple'></Bars2Icon>
+                )}
+              </Disclosure.Button>
+            </div>
+            <div className='md:flex-1 flex justify-center'>
+              <Image
+                className='h-16 w-16 md:h-20 md:w-20'
+                src={logo}
+                alt='white L inside a black circle'
+              />
+            </div>
+            <div className='md:flex-1 hidden md:flex justify-end'>
+              <button className='py-3 px-9 bg-gray rounded-3xl transition duration-300 ease-in-out hover:scale-110 hover:text-purple hover:bg-black'>
+                Contact me
+              </button>
+            </div>
+          </div>
+
+          <Transition
+            enter='transition duration-100 ease-out'
+            enterFrom='transform scale-95 opacity-0'
+            enterTo='transform scale-100 opacity-100'
+            leave='transition duration-75 ease-out'
+            leaveFrom='transform scale-100 opacity-100'
+            leaveTo='transform scale-95 opacity-0'
+            >
+            <Disclosure.Panel  className='bg-black absolute inset-0 h-screen'>
+              <div>adsasds</div>
+            </Disclosure.Panel>
+          </Transition>
+        </>
+      )}
+    </Disclosure>
+  );
+}
