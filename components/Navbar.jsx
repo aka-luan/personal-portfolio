22146100a75f819a83fadd05fa177c9b66cf7e1a
Luan Felipe Britto Alves <lfalves.terc@pa.sebrae.com.br>
2023-04-24 17:01:30 -0300

    positioning, space and size adjustments

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,7 @@ import logo from '/public/assets/logo.svg';
 import Image from 'next/image';
 import { Bars2Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Disclosure, Transition } from '@headlessui/react';
+import Button from './Button';
 
 const navigation = [
   { name: 'Home', href: '#', current: true },
@@ -18,12 +19,12 @@ const Navbar = () => {
     <Disclosure as='nav'>
       {({ open }) => (
         <>  
-          <div className='flex px-6 py-3 justify-between items-center text-black font-medium'>
+          <div className='flex px-5 py-3 justify-between items-center text-black'>
             <div className='md:flex-1 hidden md:block'>
-              <div className='flex xl:space-x-4'>
+              <div className='flex space-x-6 xl:space-x-10'>
                 {navigation.map((item) => (
                   <a
-                    className='hover:text-purple px-3 py-2 transition duration-300 ease-in-out'
+                    className='hover:text-purple py-2 transition duration-300 ease-in-out'
                     key={item.name}
                     href={item.href}
                     aria-current={item.current ? 'page' : undefined}>
@@ -33,10 +34,10 @@ const Navbar = () => {
               </div>
             </div>
             <div className='md:hidden'>
-              <Disclosure.Button className={open ? `toggle-btn` : ''}>
-                <span className='bg-black w-8 h-1 rounded absolute top-10 -mt-0.5 transition-all duration-500 -translate-y-3'></span>
-                <span className='bg-black w-8 h-1 rounded absolute top-10 -mt-0.5 transition-all duration-500 -translate-x-2'></span>
-                <span className='bg-black w-8 h-1 rounded absolute top-10 -mt-0.5 transition-all duration-500 translate-y-3'></span>
+              <Disclosure.Button className={` ${open ? `toggle-btn` : ''}`}>
+                <span className='bg-black w-8 h-1 rounded absolute top-11 -mt-0.5 transition-all duration-500 -translate-y-3'></span>
+                <span className='bg-black w-8 h-1 rounded absolute top-11 -mt-0.5 transition-all duration-500 -translate-x-2'></span>
+                <span className='bg-black w-8 h-1 rounded absolute top-11 -mt-0.5 transition-all duration-500 translate-y-3'></span>
                 {/* {open ? (
                   <XMarkIcon className='h-8 w-8 text-black cursor-pointer hover:text-purple'></XMarkIcon>
                 ) : (
@@ -46,15 +47,13 @@ const Navbar = () => {
             </div>
             <div className='md:flex-1 flex justify-center'>
               <Image
-                className='h-14 w-14 md:h-20 md:w-20'
+                className='h-16 w-16 md:h-20 md:w-20'
                 src={logo}
-                alt='white L inside a black circle'
+                alt='white letter L inside a black circle'
               />
             </div>
             <div className='md:flex-1 hidden md:flex justify-end'>
-              <button className='py-3 px-9 bg-gray rounded-3xl transition duration-300 ease-in-out hover:scale-110 hover:text-purple hover:bg-black'>
-                Contact me
-              </button>
+              <Button className={'bg-black text-white'}>Contact me</Button>
             </div>
           </div>
 
@@ -67,7 +66,7 @@ const Navbar = () => {
             leaveTo="opacity-0"
           >
             <Disclosure.Panel className='bg-white absolute top-68 w-full origin-top'>
-              <div className='flex flex-col gap-6 justify-center items-center min-h-[calc(100vh-80px)]'>
+              <div className='flex flex-col gap-6 justify-center items-center min-h-[calc(100vh-120px)]'>
                 {navigation.map((item) => (
                   <a
                     className='hover:text-purple font-semibold text-3xl px-3 py-2 transition duration-300 ease-in-out'
