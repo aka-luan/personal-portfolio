d91d3ba7c8dcab5d9c176c37861bd01d5dba9883
Luan Felipe Britto Alves <lfalves.terc@pa.sebrae.com.br>
2023-04-25 17:04:29 -0300

    adjusted mobile layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={`${nunito.className} bg-white overflow-hidden`}>
-        <div className='lg:container relative :max-w-screen max-[767px]:overflow-hidden xl:max-w-9xl'>
+        <div className='lg:container relative max-w-full lg:m-auto max-[767px]:overflow-hidden xl:max-w-9xl'>
           <Navbar />
           {children}
         </div>
