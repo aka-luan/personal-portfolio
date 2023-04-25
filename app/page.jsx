1cf67a5ace5d0c5dfbf061a01d7fe71efecf74b1
Luan Felipe Britto Alves <lfalves.terc@pa.sebrae.com.br>
2023-04-25 17:09:39 -0300

    added purple bar to main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,34 @@
 import Button from "@/components/Button";
 
 const stack = [
-  {name: 'html', classNames: 'bg-purple opacity-50 text-white'},
-  {name: 'javascript', classNames: 'bg-purple opacity-100 text-white'},
-  {name: 'react', classNames: 'bg-black opacity-100 text-white'},
-  {name: 'css', classNames: 'bg-gray opacity-100 text-black'}
+  { name: 'html', classNames: 'bg-purple opacity-50 text-white' },
+  { name: 'javascript', classNames: 'bg-purple opacity-100 text-white' },
+  { name: 'react', classNames: 'bg-black opacity-100 text-white' },
+  { name: 'css', classNames: 'bg-gray opacity-100 text-black' }
 ]
 
 export default function Home() {
   return (
     <main className="px-5">
-      <div id="header" className="flex flex-col-reverse lg:flex-row">
-        <div id="header-info" className="flex-col w-[30rem]">
-          <h1 className="text-7xl xl:text-8xl mt-16 mb-6">Meet <b>Luan</b></h1>
-          <h5 className="text-base mb-10">Javascript developer with over 5 years of experience</h5>
-          <Button className='bg-purple text-white mb-40'>Hire me</Button>
-          <div className="flex flex-wrap gap-2">
+      <div id="header" className="flex flex-col-reverse md:flex-row h-[45rem]">
+        <div id="header-info" className="flex flex-col w-full lg:w-[30rem] justify-between">
+          <div className="space-y-8">
+            <h1 className="text-7xl xl:text-8xl mt-24 mb-6">Meet <span className="before:block before:absolute before:-inset-1 
+          before:-skew-y-3 before:bg-purple relative
+           inline-block"><b className="relative text-white">Luan</b></span></h1>
+            <h5 className="text-base ">Passionate JavaScript developer with over 5 years of experience</h5>
+            <Button className='bg-purple text-white mb-40'>Hire me</Button>
+          </div>
+
+          <div className="flex flex-wrap gap-3">
             {stack.map((tech) => (
-              <div key={tech.name} className={` px-7 py-3 rounded-xl text-4xl ${tech.classNames}`}>{tech.name}</div>
+              <div key={tech.name} className={`px-7 py-3 rounded-xl text-4xl ${tech.classNames}`}>{tech.name}</div>
             ))}
           </div>
         </div>
-        <div id="header-img">img</div>
+        <div id="header-img" className="h-full">
+          <span className="absolute -z-10 inset-y-0 right-0 -top-52 rounded-full w-[30rem] h-[70rem] rotate-45 bg-purple"></span>
+        </div>
       </div>
     </main>
   )
